f3d9f8d5ea0661cc55c5be3e9bc48671da8595d9
admin <a@a.a>
2023-10-20 12:13:29 +0300

    first commit

---
+++ Takowane na kod/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Takowane_na_kod
+{
+    internal class Program
+    {
+        public const string fileName = "example.bim";
+        static void Main(string[] args)
+        {
+            FileStream fileStream=new FileStream(fileName, FileMode.Create);
+            BinaryWriter binaryWriter=new BinaryWriter(fileStream);
+            for (int i = 0; i < 11; i++)
+            {
+                binaryWriter.Write(i);     
+            }
+            binaryWriter.Close();
+            fileStream.Close();
+            fileStream=new FileStream(fileName,FileMode.Open,FileAccess.Read);
+            BinaryReader binaryReader=new BinaryReader(fileStream);
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine(binaryReader.ReadInt32());
+            }
+            binaryReader.Close();
+            fileStream.Close();
+        }
+        
+
+    }
+}
---
+++ Takowane na kod/Takowane na kod/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Takowane_na_kod
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
